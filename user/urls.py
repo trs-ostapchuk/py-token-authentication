dd8cad01f7ad3443baf4fd5f6e05c801d77f03b1
Taras <tarasostapchuk27@gmail.com>
2026-01-12 19:49:07 +0100

    Modify login endpoint

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="token"),
+    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
