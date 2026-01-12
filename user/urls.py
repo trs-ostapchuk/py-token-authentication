9e8d716d6974d3f2a5619ce276c1690f163d7fb8
Taras <tarasostapchuk27@gmail.com>
2026-01-12 19:40:30 +0100

    Modify endpoint for token auth

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
-from rest_framework.authtoken import views
+from user.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", views.obtain_auth_token, name="token"),
+    path("login/", CreateTokenView.as_view(), name="token"),
 ]
 
 app_name = "user"
