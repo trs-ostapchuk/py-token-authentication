a7d902f0bd12c01f0ddbed211487240548875b4c
Taras <tarasostapchuk27@gmail.com>
2026-01-12 19:38:38 +0100

    Add endpoint for token auth

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from user.views import CreateUserView
+from rest_framework.authtoken import views
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", views.obtain_auth_token, name="token"),
 ]
 
 app_name = "user"
