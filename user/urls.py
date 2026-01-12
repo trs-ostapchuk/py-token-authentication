44c9cbd0074a53dd35585d797c02f301cfe1346d
Taras <tarasostapchuk27@gmail.com>
2026-01-12 19:36:47 +0100

    Add endpoint for registration user

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,9 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create")
+]
+
+app_name = "user"
